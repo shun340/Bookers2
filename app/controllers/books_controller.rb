0b4553b914f372c9feb49e882e34940159ddea16
shun340 <92897220+shun340@users.noreply.github.com>
2021-11-23 21:25:59 +0000

    [Update]4-10

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -26,6 +26,11 @@ class BooksController < ApplicationController
 
   def edit
     @book = Book.find(params[:id])
+    if @book.user == current_user
+    　render "edit"
+    else
+      redirect_to books_path
+    end
   end
 
   def update
@@ -49,4 +54,5 @@ class BooksController < ApplicationController
   def book_params
     params.require(:book).permit(:title, :body)
   end
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,11 @@ class UsersController < ApplicationController
 
   def edit
     @user = User.find(params[:id])
+    if @user == current_user
+    　render "edit"
+    else
+      redirect_to user_path(current_user.id)
+    end
   end
 
   def update
