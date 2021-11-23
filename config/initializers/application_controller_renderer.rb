c4568f491b3383e94393882da20a4fe2da0451c7
shun340 <92897220+shun340@users.noreply.github.com>
2021-11-23 15:00:03 +0000

    [Finish]4-10

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '251ffbe5d00b1df9edf389c6893a880ab6f21db6f752f217314f3cf81a60c6bd574ef08b97ec743e1b3383f4299975594284144ff9650b3c6a792ff75fed5c99'
