2996d082c43477a725462d7d8c088a3baa5487da
shun340 <92897220+shun340@users.noreply.github.com>
2021-11-24 03:18:57 +0000

    [Update]4-10_ver1.25

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'homes#top'
 
+  get 'home/about' => 'homes#about', as: 'about'
+
   resources :books, only: [:new, :create, :index, :show, :edit, :update, :destroy]
 
   resources :users, only: [:index, :show, :edit, :update]
